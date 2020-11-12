7fef20165a3c460721cc641bb27fe48decec67c4
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-12 15:27:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int balance = 100;
         int replenishment = 1800;
         int factor = 100;
         int bonus = replenishment / factor;
         int total = balance + replenishment + bonus;
-        if (replenishment < 1000){;
-                total = balance + replenishment;
+        if (replenishment < 1000) {
+            total = balance + replenishment;
         }
         System.out.println(total);
-
 
 
     }
